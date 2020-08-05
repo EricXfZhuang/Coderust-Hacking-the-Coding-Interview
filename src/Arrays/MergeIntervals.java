2d89240a643937c3c31630251d9c25f323270f45
EricZhuang9615 <xiaofzhuang@gmail.com>
2020-08-05 17:27:26 +0800

    8/5 commit

--- src/Arrays/MergeIntervals.java
+++ src/Arrays/MergeIntervals.java
@@ -21,10 +21,33 @@ public class MergeIntervals {
         ArrayList<Pair> result = new ArrayList<Pair>();
         // write your code here
         int len = v.size();
-        int end = v.get(len-1).second;
-        while(true){
-
+        Pair prev = v.get(0);
+        for(int i = 1; i < len; i++){
+            Pair curr = v.get(i);
+            if(prev.second >= curr.first){
+                prev.second = Math.max(prev.second, curr.second);
+            }else{
+                result.add(prev);
+                prev = curr;
+            }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Pair> v = new ArrayList<Pair>();
+
+        v.add(new Pair(1, 5));
+        v.add(new Pair(3, 7));
+        v.add(new Pair(4, 6));
+        v.add(new Pair(6, 8));
+        v.add(new Pair(10, 12));
+        v.add(new Pair(11, 15));
+
+        ArrayList<Pair> result = mergeIntervals(v);
+
+        for(int i=0; i<result.size(); i++){
+            System.out.print(String.format("[%d, %d] ", result.get(i).first, result.get(i).second));
+        }
     }
 }
