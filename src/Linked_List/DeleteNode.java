6cd389270efc4bbe337ce1b6a0dd41721f211d04
EricZhuang9615 <xiaofzhuang@gmail.com>
2020-08-06 17:30:48 +0800

    8/6 commit

--- src/Linked_List/DeleteNode.java
+++ src/Linked_List/DeleteNode.java
@@ -5,9 +5,25 @@ package Linked_List;
  * @Date 2020/8/6
  */
 public class DeleteNode {
-    public static LinkedListNode deleteNode(LinkedListNode head, int key)
-    {
+    public static LinkedListNode deleteNode(LinkedListNode head, int key) {
         //TODO: Write - Your - Code
+        LinkedListNode curr = head;
+        LinkedListNode prev = head;
+        while(curr != null){
+            if(curr.data == key) {
+                if (curr == head) {
+                    head = head.next;
+                    return head;
+                }else{
+                    prev.next = curr.next;
+                    curr.next = null;
+                    return head;
+                }
+            }else{
+                prev = curr;
+                curr = curr.next;
+            }
+        }
         return head;
     }
 }
