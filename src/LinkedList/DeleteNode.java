535dde593ae6de7999586296f2b729330a02b654
EricZhuang9615 <xiaofzhuang@gmail.com>
2020-08-06 21:34:14 +0800

    Insertion Sort

--- src/LinkedList/DeleteNode.java
+++ src/LinkedList/DeleteNode.java
@@ -13,17 +13,28 @@ public class DeleteNode {
             if(curr.data == key) {
                 if (curr == head) {
                     head = head.next;
-                    return head;
                 }else{
                     prev.next = curr.next;
                     curr.next = null;
-                    return head;
                 }
+                return head;
             }else{
                 prev = curr;
                 curr = curr.next;
             }
         }
         return head;
+    }
+
+    public static void main(String[] args) {
+        LinkedListNode listHead = null;
+        int[] list1 = {20, 14,36, 11, 72, 41};
+        listHead = LinkedList.createLinkedList(list1);
+        System.out.print("Original: ");
+        LinkedList.display(listHead);
+
+        listHead = deleteNode(listHead, 72);
+        System.out.printf("After deleting 72 from list: ");
+        LinkedList.display(listHead);
     }
 }
---
+++ src/LinkedList/InsertionSort.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+
+/*
+* Given the head pointer of a linked list, sort it in ascending order using insertion sort.
+*
+* */
+public class InsertionSort {
+    public static LinkedListNode insertionSort(LinkedListNode head) {
+        //TODO: Write - Your - Code
+        if(head == null || head.next == null) return head;
+
+        LinkedListNode curr = head.next;
+        LinkedListNode insert = head;
+        while(curr != null){
+
+            while(){
+
+            }
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
