edac714830d8916f8d41a28339b7a1053e616e3e
EricZhuang9615 <xiaofzhuang@gmail.com>
2020-08-09 17:37:40 +0800

    8/9 commit

--- src/LinkedList/MergeSort.java
+++ src/LinkedList/MergeSort.java
@@ -1,23 +1,112 @@
 package LinkedList;
 
+import javax.sound.sampled.Line;
+
 public class MergeSort {
     public static LinkedListNode mergeSort(LinkedListNode head) {
         //TODO: Write - Your - Code
-        LinkedListNode slow = head;
-        LinkedListNode fast = head;
-
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next;
+        if(head == null || head.next == null){
+            return head;
         }
 
-        if(slow != null){
-            mergeSort(slow);
+        Pair<LinkedListNode> pair = splitHalf(head);
+
+        pair.first = mergeSort(pair.first);
+        pair.second = mergeSort(pair.second);
+
+        return merge(pair.first, pair.second);
+    }
+
+    public static Pair<LinkedListNode> splitHalf(LinkedListNode head){
+        if(head == null){
+            return null;
         }
-        return head;
+
+
+       if(head.next == null){
+           return new Pair<LinkedListNode>(head, null);
+       }else{
+           LinkedListNode slow = head, fast = head.next;
+
+           while(fast != null){
+               fast = fast.next;
+
+               if (fast != null) {
+
+                   fast = fast.next;
+                   slow = slow.next;
+               }
+           }
+
+           Pair<LinkedListNode> pair = new Pair<LinkedListNode>(head, slow.next);
+           slow.next = null;
+
+           return pair;
+       }
     }
 
     public static LinkedListNode merge(LinkedListNode leftHead, LinkedListNode rightHead){
+        LinkedListNode newHead = null;
 
+        if(leftHead == null){
+            return rightHead;
+        }else if(rightHead == null){
+            return leftHead;
+        }
+
+        if(leftHead.data <= rightHead.data){
+            newHead = leftHead;
+            leftHead = leftHead.next;
+        }else{
+            newHead = rightHead;
+            rightHead = rightHead.next;
+        }
+
+        LinkedListNode curr = newHead;
+
+        while(leftHead != null && rightHead != null){
+            LinkedListNode temp = null;
+            if(leftHead.data <= rightHead.data){
+                temp = leftHead;
+                leftHead = leftHead.next;
+            }else{
+                temp = rightHead;
+                rightHead = rightHead.next;
+            }
+            curr.next = temp;
+            curr = curr.next;
+        }
+
+        if(leftHead != null){
+            curr.next = leftHead;
+        }
+        if(rightHead != null){
+            curr.next = rightHead;
+        }
+
+        return newHead;
+    }
+
+    static class Pair<E>{
+        public E first;
+        public E second;
+
+        public Pair(E first, E second){
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] v1 = {29, 23, 82, 11, 4, 3, 21};
+        LinkedListNode listHead1 = LinkedList.createLinkedList(v1);
+
+        System.out.print("Unsorted list: ");
+        LinkedList.display(listHead1);
+
+        listHead1 = mergeSort(listHead1);
+        System.out.print("Sorted list: ");
+        LinkedList.display(listHead1);
     }
 }
